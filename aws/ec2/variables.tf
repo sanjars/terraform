fb178a11f8f1643db4c413363fd9e337a0bdb978
Sanjar <sanjar.dzholdoshov@slalom.com>
2019-12-22 12:03:11 -0600

    updated vars file

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -2,22 +2,11 @@
 Variables
 *********/
 variable "aws-region" {
-  type    = "string"
-  default = "us-east-1"
+  type    = "string" 
 }
-variable "prefix" {
-  default = "terra"
-}
-variable "email" {
-  default = "example@example.com"
-}
-variable "aws_ami" {
-  default = "ami-0a313d6098716f372"
-}
-variable "instance_type" {
-  default = "t2.medium"
-}
-variable "instance_count" {
-  default = 3
-}
+variable "prefix" {}
+variable "email" {}
+variable "aws_ami" {}
+variable "instance_type" {}
+variable "instance_count" {}
 
