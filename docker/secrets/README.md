91ed1bb05adcaef9b4f65d0eca6501447c3dab42
Sanjar Sapar <sanjar.devops@gmail.com>
2020-04-06 14:50:25 -0500

    creating secrets

---
+++ docker/secrets/README.md
@@ -0,0 +1,4 @@
+Encode the password with base64
+```bash
+echo 'my_password_here' | base64
+```
---
+++ docker/secrets/variables.tf
@@ -0,0 +1,8 @@
+variable "mysql_root_password" {
+    default = "QGNvdmlkMTkyMDIwCg=="  # encrypted pass goes here
+}
+
+variable "mysql_db_password" {
+  default = "QGNvdmlkMTkyMDIwCg=="    # encrypted pass goes here
+}
+
